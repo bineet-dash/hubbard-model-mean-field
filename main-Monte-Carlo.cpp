d318f4b80660a995e27cc4025fa10d65e6e5a45e
Bineet Dash <bineetdash@gmail.com>
2018-01-13 09:22:51 +0530

    Observable averaged over last few sweeps, filename includes no of sweeps

--- main-Monte-Carlo.cpp
+++ main-Monte-Carlo.cpp
@@ -14,9 +14,9 @@ int main(int argc, char* argv[])
   // int final_exp, initial_exp;
   // cout << "Enter the number of MC sweeps, final and initial exponent: ";
   // cin >> no_sweeps >> final_exp >> initial_exp;
-  int no_sweeps = 1000;
-  int initial_exp = -3;
-  int final_exp = 1;
+  int no_sweeps = 10000;
+  int initial_exp = -2;
+  int final_exp = 0;
 
   double final_temp = 10*pow(10,final_exp);
 
@@ -41,7 +41,7 @@ int main(int argc, char* argv[])
   suggested_Mc = suggested_Mcx = suggested_Mcy = suggested_Mcz = MatrixXcd::Zero(2*size,2*size);
 
   string filename, latticedata;
-  latticedata = "_U="+to_string(int(U))+"_size="+to_string(size);
+  latticedata = "_U="+to_string(int(U))+"_size="+to_string(size)+"_sweeps="+to_string(no_sweeps);
   filename="wolframscripts/spin_arrangement"+current_time_str()+latticedata+".nb"; ofstream outfile_spinarr(filename);
   spinarrangement_Mathematica_output(randsigma,outfile_spinarr);
   filename="data/m_length_vs_temp"+ current_time_str()+latticedata+".txt"; ofstream outfile_mlength(filename);
@@ -49,17 +49,12 @@ int main(int argc, char* argv[])
   // filename="data/mcdetails"+current_time_str()+latticedata+".txt"; ofstream outfile_mcdetails(filename);
   cout << "==============================\n"<< "filename is: " << filename << "\n========================\n";
 
-  assert(no_sweeps%2==0);
-  double final_free_energy, count_free_energy;
-  double uniform_rv, move_prob, m_length;
-
   for(int j=final_exp; j>=initial_exp; j--)
   {
-    for(int i=10; i>=2; i--)
+    for(double i=10; i>=1.1; i-=0.1)
     {
       double temperature = i*pow(10,j);
-      final_free_energy = count_free_energy = 0;
-      for(int sweep=0; sweep<no_sweeps; sweep++)
+      for(int sweep=0; sweep<0.75*no_sweeps; sweep++)
       {
         for(int lattice_index=0; lattice_index<size; lattice_index++)
         {
@@ -70,31 +65,56 @@ int main(int argc, char* argv[])
           MatrixXcd suggested_Hamiltonian = Mc-U/2*(suggested_Mcx+suggested_Mcy+suggested_Mcz);
           double suggested_free_energy = find_free_energy(suggested_Hamiltonian, temperature, suggested_randsigma);
 
-          uniform_rv = ran0(&idum); move_prob = exp((free_energy - suggested_free_energy)/temperature);
+          double uniform_rv = ran0(&idum); double move_prob = exp((free_energy - suggested_free_energy)/temperature);
           if(uniform_rv <= move_prob)
           {
-            // outfile_mcdetails << "accepted: Temp=" << temperature << " Sweep= " << sweep << " uniform_rv="<< uniform_rv << " move_prob=" << move_prob << " free_energy=" << free_energy << " suggested_free_energy= " << suggested_free_energy << endl;
             free_energy = suggested_free_energy;
             randsigma = suggested_randsigma;
           }
           else
           {
-            // outfile_mcdetails << "rejected: Temp=" << temperature << " Sweep= " << sweep << " uniform_rv="<< uniform_rv << " move_prob=" << move_prob << " free_energy=" << free_energy << " suggested_free_energy= " << suggested_free_energy << endl;
             suggested_randsigma=randsigma;
           }
         }
       }
+
+      double final_free_energy = 0; double count_free_energy = 0;
+      for(int sweep= int(0.75*no_sweeps); sweep<no_sweeps; sweep++)
+      {
+        for(int lattice_index=0; lattice_index<size; lattice_index++)
+        {
+          sigma_generate(suggested_randsigma, lattice_index, idum, temperature);
+          matrixelement_sigmax(suggested_Mcx, suggested_randsigma);
+          matrixelement_sigmay(suggested_Mcy, suggested_randsigma);
+          matrixelement_sigmaz(suggested_Mcz, suggested_randsigma);
+          MatrixXcd suggested_Hamiltonian = Mc-U/2*(suggested_Mcx+suggested_Mcy+suggested_Mcz);
+          double suggested_free_energy = find_free_energy(suggested_Hamiltonian, temperature, suggested_randsigma);
+
+          double uniform_rv = ran0(&idum); double move_prob = exp((free_energy - suggested_free_energy)/temperature);
+          if(uniform_rv <= move_prob)
+          {
+            free_energy = suggested_free_energy;
+            randsigma = suggested_randsigma;
+          }
+          else
+          {
+            suggested_randsigma=randsigma;
+          }
+        }
+        if(sweep%50==0) {final_free_energy += free_energy; count_free_energy++;}
+      }
+
       outfile_mlength << temperature << " ";
       double m_length_avg=0;
       for(int j=0; j<size; j++)
       {
-        m_length = 0;
+        double m_length = 0;
         for( int k=0; k<3; k++) m_length += pow(randsigma(j,k),2);
         outfile_mlength << " " << sqrt(m_length)  << " ";
         m_length_avg+= sqrt(m_length);
       }
       outfile_mlength << " \t" << m_length_avg/double(size) <<  endl;
-      outfile_freeenergy << temperature << " " << free_energy << endl;
+      outfile_freeenergy << temperature << " " << final_free_energy/double(count_free_energy) << endl;
 
       cout << "\rtemperature = " << temperature << " done."; cout.flush();
       // progress_percent_desc(initial_temp, final_temp, temperature);
